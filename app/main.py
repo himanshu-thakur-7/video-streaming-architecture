f80ffc172edcbbcb7c0207eed8fe74f3e0f5843c
Himanshu <himan7781@gmail.com>
2026-05-08 07:38:58 +0530

    added video model and endpoints

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+from app.models import Video,VideoCreateRequest,create_video
+from app.db import videos_db
 
 app = FastAPI()
 
@@ -10,3 +12,25 @@ def healthCheck():
 def healthCheck():
     return {"status":"ready"}
 
+@app.post("/videos/initiate-upload", response_model=Video)
+def initiate_upload(request: VideoCreateRequest):
+    video = create_video(
+        title=request.title,
+        description=request.description
+    )
+    videos_db[video.id] = video
+
+    print(f"videos db: {videos_db}")
+    return video
+
+
+
+@app.get("/videos/{video_id}",response_model=Video)
+def get_video(video_id:str)->Video:
+    print(f"videos db: {videos_db}")
+    video = videos_db.get(video_id)
+
+    if not video:
+        raise HTTPException(status_code=404,detail="Video Not Found")
+
+    return video
---
+++ app/models.py
@@ -0,0 +1,33 @@
+from enum import Enum
+from pydantic import BaseModel
+from uuid import uuid4
+from datetime import datetime
+
+class VideoStatus(str,Enum):
+    CREATED="CREATED"
+    PROCESSING="PROCESSING"
+    UPLOADING="UPLOADING"
+    READY="READY"
+    FAILED="FAILED"
+
+
+class Video(BaseModel):
+    id:str
+    title:str
+    description:str | None = None
+    createdAt : datetime
+    status: VideoStatus
+
+class VideoCreateRequest(BaseModel):
+    title: str
+    description : str | None = None
+
+def create_video(title: str, description: str | None = None) -> Video: 
+    return Video(
+        id=str(uuid4()),
+        title=title,
+        description=description,
+        createdAt=datetime.utcnow(),
+        status=VideoStatus.CREATED
+    )
+
